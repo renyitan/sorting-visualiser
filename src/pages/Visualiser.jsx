6c19949334aae1b0c893c9742242c97e5806e66d
Renyi Tan <tanrenyi@gmail.com>
2020-05-06 21:17:56 +0800

    too lazy today

---
+++ src/pages/Visualiser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Visualiser.css';
+
+const Visualiser = () => {
+  return (
+    <div>
+      Hello
+      <div className="array-container"></div>
+    </div>
+  );
+};
+
+export default Visualiser;
